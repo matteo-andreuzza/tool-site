823ab4525e39688c1a537807a6bc9e1375c4c46c
matteo-andreuzza <76852004+matteo-andreuzza@users.noreply.github.com>
2022-02-26 22:19:10 +0100

    pagine d'esempio, sliders da sistemare e immagini

---
+++ categorie/ufficio/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <script async src="https://unpkg.com/typer-dot-js@0.1.0/typer.js"></script>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/23bf9360c6.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    <title>Tool senza fine</title>
+    <title>Ufficio - Tool senza fine</title>
+</head>
+
+<body>
+    <?php 
+    require navbar.php;
+    include navbar.php;
+    ?>
+    <div class="intestazione">
+        <h1 class="titolih1">Tool per l'ufficio</h1>
+        <h2 class="titolih2">Sono tanti i tool che ci permettono di facilitare il lavoro di ufficio</h2>
+        <h2 class="titolih2">dalla gestione dei fogli di calcolo ai database</h2>
+    </div>
+
+</body>
+
+</html>
